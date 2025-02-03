50943832fc28f5a3c7022a0178df6fa55eee4f22
HugoPVoodoo <hugo.peyron@voodoo.io>
2025-02-03 16:16:42 +0100

    Initial Commit

---
+++ myDay/myDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  myDayApp.swift
+//  myDay
+//
+//  Created by Hugo Peyron on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct myDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
